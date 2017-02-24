4209354a2a2afbd1dd23f19a5c7885086eec0ae9
Saradhi Indurthi <saradhix@yahoo.com>
2017-02-24 10:07:07 +0530

    Generate urls

--- make_text_files.py
+++ make_text_files.py
@@ -6,12 +6,15 @@ def main():
 
   raw_weird=[]
   raw_normal=[]
+  weird_urls=[]
+  normal_urls=[]
   #Load weird news
   fd = open(weird_news, 'r')
   for line in fd:
     json_obj = json.loads(line)
     title = ''.join([i if ord(i) < 128 else ' ' for i in json_obj['title']])
     raw_weird.append(str(title))
+    weird_urls.append(json_obj['url'])
 
   fd.close()
   print( "Weird news items :", len(raw_weird))
@@ -22,6 +25,7 @@ def main():
     json_obj = json.loads(line)
     title = ''.join([i if ord(i) < 128 else ' ' for i in json_obj['title']])
     raw_normal.append(str(title))
+    normal_urls.append(json_obj['url'])
   fd.close()
 
   #Load stopwords
@@ -47,6 +51,14 @@ def main():
         continue
       words[word]=words.get(word,0)+1
 
+  fd.close()
+  fd =open('weird_urls.txt','w')
+  for url in weird_urls:
+    fd.write(url+'\n')
+  fd.close()
+  fd =open('normal_urls.txt','w')
+  for url in normal_urls:
+    fd.write(url+'\n')
   fd.close()
 
   s=sorted(words.items(), key=lambda x:x[1], reverse=True)
