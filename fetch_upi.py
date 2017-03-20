8e1a06698798a62dd0630b33d34fcae0461633f2
Vijayasaradhi Indurthi <vi186001@teradata.com>
2017-03-20 02:21:48 -0700

    Fetch upi news

---
+++ fetch_upi.py
@@ -0,0 +1,31 @@
+from bs4 import BeautifulSoup
+import requests
+import json
+import sys
+
+
+start_page=1
+end_page=126
+
+baseurl='http://www.upi.com/Odd_News/2001/p'
+
+
+for page in range(start_page, end_page+1):
+  url = baseurl+str(page)
+  #print "Trying ", url
+
+
+
+  r=requests.get(url)
+  data=r.text
+  soup=BeautifulSoup(data,"html.parser")
+
+  for item in soup.find_all("div", { "class" : "upi_item" }):
+    link = item.contents[1].get('href')
+    title = item.contents[1].get('title')
+    #title = link.contents[0].text.encode('utf8')
+    #link = baseurl+link.contents[0].get('href').encode('utf8')
+    #print title,',',link
+
+    jsonobj={'title':title,'url':link}
+    print json.dumps(jsonobj)
